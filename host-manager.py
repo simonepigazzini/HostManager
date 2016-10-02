8f76c88793063c5ca6033b1e339dd240669f5c35
Simone Pigazzini <simone.pigazzini@cern.ch>
2016-10-02 14:13:10 +0200

    first commit

---
+++ host-manager.py
@@ -0,0 +1,32 @@
+#!/bin/python
+
+import sqlite3
+
+db = sqlite3.connect('data/customer.db')
+cursor = db.cursor()
+
+cursor.execute(
+    '''
+    CREATE TABLE customers(id INTEGER PRIMARY KEY, fullname TEXT, agency TEXT, nights INTEGER)
+    '''
+    )
+
+db.commit()
+
+new_customer = {'fullname':'Alessia', 'nights': 3}
+
+cursor.execute(
+    '''
+    INSERT INTO customers(fullname, nights)
+    VALUES(:fullname, :nights)
+    ''',
+    new_customer)
+
+db.commit()
+
+cursor.execute('''SELECT fullname, agency, nights FROM customers''')
+all_customers = cursor.fetchall()
+
+for customer in all_customers:
+    print(customer)
+                           
